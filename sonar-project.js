c90cee1de992b9dc7eb20ac2a054b704d4e8a10b
tahmichael25 <65462993+tahmichael25@users.noreply.github.com>
2023-07-13 05:54:39 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://54.187.20.177:9000/',
+  serverUrl: 'http://35.93.58.131:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
